dddcbbc88ed3331a6b849f734ed1e56ca4f66d87
Kathleen Letsch <cskathi@web.de>
2025-01-29 19:00:03 +0100

    Iniital commit

---
+++ src/main/java/de/kathibrati/dishtracker/DishTrackerApplication.java
@@ -0,0 +1,13 @@
+package de.kathibrati.dishtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DishTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DishTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/kathibrati/dishtracker/DishTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kathibrati.dishtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DishTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
